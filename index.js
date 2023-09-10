9ac5b9771eeb51491b5dec8b3fea00d442820f6a
Muhammad Hanzala <muhammadhanzala214@gmail.com>
2023-09-10 20:17:51 +0500

    Bubble game v1

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+var num = 0;
+var timer = 60;
+
 function makeBubble() {
   var clutter = "";
   function randomNumber() {
-    return Math.floor(Math.random() * 10);
+    return (numb = Math.floor(Math.random() * 10));
   }
   for (let index = 0; index < 198; index++) {
     clutter += `<div class="bubble">${randomNumber()}</div>`;
@@ -9,31 +12,45 @@ function makeBubble() {
   document.querySelector("#pbtm").innerHTML = clutter;
 }
 
-function timer() {
-  var timer = 60;
+function runTimer() {
+  // Display the initial value
+  document.querySelector("#timerval").textContent = timer;
 
-  function runTimer() {
-    // Display the initial value
-    document.querySelector("#timerval").textContent = timer;
-
-    var interval = setInterval(() => {
-      if (timer > 0) {
-        timer--;
-        document.querySelector("#timerval").textContent = timer;
-      } else {
-        clearInterval(interval); // Stop the timer when it reaches 0
-      }
-    }, 1000);
-  }
-  // Call the runTimer function to start the timer
-  runTimer();
+  var interval = setInterval(() => {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timerval").textContent = timer;
+    } else {
+      clearInterval(interval); // Stop the timer when it reaches 0
+    document.querySelector("#pbtm").innerHTML = "";
+    }
+  }, 1000);
 }
+// Call the runTimer function to start the timer
 
 function getNewHit() {
-    let num = Math.floor(Math.random()*10);
-    document.querySelector("#hitval").textContent = num;
+  num = Math.floor(Math.random() * 10);
+  document.querySelector("#hitval").textContent = num;
 }
 
-// getNewHit();
-// makeBubble();
-// timer();
+function increaseScore() {
+  var score = 0;
+  function escore() {
+    score += 1;
+    document.querySelector("#score").textContent = score;
+  }
+  escore();
+}
+
+document.querySelector("#pbtm").addEventListener("click", function (dets) {
+  var clickedNum = Number(dets.target.textContent);
+  if (clickedNum === num) {
+    increaseScore();
+    getNewHit();
+    makeBubble();
+  }
+});
+
+getNewHit();
+makeBubble();
+runTimer();
